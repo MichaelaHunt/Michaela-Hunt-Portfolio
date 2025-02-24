8648886885bba23445cebb094697e23abff4819e
Michaela Hunt <micapplehunt@gmail.com>
2025-02-24 13:27:19 -0500

    Remove Vite logo, add resume!

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,10 +1,12 @@
-
+import myResume from '/resume.pdf';
 
 function Resume() {
     return (
         <>
-            <div style={{ flex: 1, display: "flex", justifyContent: "center", alignItems: "center" }}>
-                <h1 style={{ textAlign: "center" }}>Coming Soon!</h1>
+            <div className='resumePage column'>
+                {/* <h1 style={{ textAlign: "center" }}>Coming Soon!</h1> */}
+                <h2 style={{margin: "24px 0"}}>Don't see anything? Try opening in another browser, or download it <u><a href="/resume.pdf" download="MichaelaHunt_Resume.pdf">here</a></u></h2>
+                <iframe src={myResume}></iframe>
             </div>
         </>
     );
